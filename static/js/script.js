e8e67803a6e3feec7a7131d8d1a4d56c94699eda
Justin Lu <wabasabi359@gmail.com>
2020-08-15 10:30:14 -0400

    add method selection

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,10 @@
-// $("#submit").off().click(function(){
-    
-    // $.post({
-    //     url: "/",
-    //     data: {'data':time.value}
-    // });
-// });
+for (let icon of document.getElementsByClassName("transit-method")){
+	icon.addEventListener('click', function(){
+	    if(this.style.opacity < 0.4){ 
+	    	this.style.opacity = "0.6";
+	    }
+	    else{
+	    	this.style.opacity = 0.2;
+	    }
+	})
+}
